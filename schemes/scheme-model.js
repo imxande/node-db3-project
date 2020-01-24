0031adf871b9a56d71a81f1e2ec2ad02f7bfae2b
imxande <alerodriguezlarosa@gmail.com>
2020-01-24 09:03:54 -0500

    findSteps method implemented and tested

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -5,7 +5,7 @@ const db = require('../data/db-config.js');
 module.exports = {
     find,
     findById,
-    // findSteps,
+    findSteps,
     // add,
     // update,
     // remove
@@ -21,4 +21,17 @@ function findById(id){
     return db('schemes')
         .where({id})
         .first()
+}
+
+// Translates to SELECT schemes.scheme_name, steps.id,
+// steps.instructions,
+// steps.step_number
+// FROM schemes 
+// JOIN steps ON steps.scheme_id = schemes.id
+// WHERE scheme_id = 5
+function findSteps(id){
+    return db('steps as s')
+        .select('s.id','e.scheme_name', 's.step_number', 's.instructions')
+        .join('schemes as e', 'e.id', 's.scheme_id' )
+        .where({scheme_id:id})
 }
